085559441fcfb5b2cefb94921702e10ad04283b5
nguakem100 <nguakem2002@yahoo.com>
2022-02-18 04:57:04 +0000

    Changes done on the 17th Feb

--- sample.py
+++ sample.py
@@ -1,5 +1,5 @@
 #This is a dummy line
 
-print (" happy Vals day to all here. how was it spent" )
+print (" happy Vals day to all here. how was it spent learning git merge" )
 print (" I had it done")
 pring (" How are u doin")
