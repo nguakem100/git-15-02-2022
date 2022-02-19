204afdb22cb95049419171b8633e7dfae1125a8c
nguakem100 <nguakem2002@yahoo.com>
2022-02-19 07:07:54 +0000

    using git revert
    This reverts commit 3320d889f79784b67410bf81675bb6b348fc9472.

--- sample.py
+++ sample.py
@@ -4,4 +4,3 @@
 print (" happy Vals day to all here. we spent it learning git merge" )
 print (" we went to the night club later night in Toronto" )
 print (" I was so tire the following day")
-print (" New subscription for Dev Branch")
